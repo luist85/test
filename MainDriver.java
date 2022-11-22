314e1397e3ba3725c189b047c5821174f21a86e2
Luis Torres <torresl85@hotmail.com>
2022-11-22 05:31:10 -0800

    added 2 + 2

--- MainDriver.java
+++ MainDriver.java
@@ -5,6 +5,9 @@ public class MainDriver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		int n1 = 2;
+		int n2 = 2;
+		System.out.println("2 + 2 = " + (n1 + n2));
 
 	}
 
